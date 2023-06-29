6c4cd1ceb5bb42391cbcee9cf15ff835851efef6
miathedev <mia@metzler.systems>
2023-06-29 13:44:07 +0200

    Test map

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <iframe allowfullscreen class="map"
+      src="//umap.openstreetmap.de/en/map/untitled-map_42476?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=true">
+    </iframe>
+    <p><a href="//umap.openstreetmap.de/en/map/untitled-map_42476">See full screen</a></p>
   </div>
 </template>
+<!-- CSS -->
+<style>
+  .map {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: none;
+  }
+</style>
